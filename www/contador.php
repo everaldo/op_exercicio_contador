1048c1a06ec9c5285ba246ed6295d461f4ab7e41
Everaldo Gomes <everaldo.gomes@gmail.com>
2015-05-22 19:26:45 +0000

    documentação - enunciado

--- www/contador.php
+++ www/contador.php
@@ -1,3 +1,33 @@
+<?php
+/* Everaldo Gomes - 22/05/2015 - Oficina de Programação
+ * everaldo.gomes@pucpr.br
+ *
+ * Contador de Visitas usando MySQL
+ *
+ *
+ * Pré-requisitos: (execute os seguintes comandos)
+ *
+ *
+ * Crie um banco de dados:
+ *
+ * create database contador;
+ *
+ *
+ * Uma tabela para armazenar as visitas:
+ *
+ * use contador;
+ * create table visitas(
+ *  total int not null default 0 primary key
+ * );
+ *
+ * Inicialize o valor de total (único registro da tabela)
+ *
+ * insert into `visitas`(total) values(0); 
+ *
+ */
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
